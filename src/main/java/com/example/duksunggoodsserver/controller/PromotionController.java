88b2c98ec9d81574f256d381fe4fd96495319c9c
Chanran33 <k55416325@gmail.com>
2021-09-26 16:56:29 +0900

    저장

---
+++ src/main/java/com/example/duksunggoodsserver/controller/PromotionController.java
@@ -0,0 +1,17 @@
+package com.example.duksunggoodsserver.controller;
+
+import com.example.duksunggoodsserver.model.entity.Promotion;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+//
+//@Slf4j
+//@RequiredArgsConstructor
+//@RestController
+//@RequestMapping("/api/home")
+//public class PromotionController {
+//}
---
+++ src/main/java/com/example/duksunggoodsserver/repository/PromotionRepository.java
@@ -0,0 +1,12 @@
+package com.example.duksunggoodsserver.repository;
+
+import com.example.duksunggoodsserver.model.entity.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PromotionRepository extends JpaRepository <Promotion, Long> {
+    List<Promotion> findAllBy(Long id);
+}
