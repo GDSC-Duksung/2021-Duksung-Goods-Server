d7738293c25395f183dd1fde6c4b7a2d66fdb7ad
sss20young <ksyt98@gmail.com>
2022-04-08 20:20:33 +0900

    chore: supplement nginx setting

---
+++ src/main/java/com/example/duksunggoodsserver/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.example.duksunggoodsserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${server.host}")
+    private String host;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(host);
+    }
+}
